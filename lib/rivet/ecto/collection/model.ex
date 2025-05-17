7a5e9f0b745fb6ff7d0bc971a3a4b0dd6a6255e7
Brandon Gillespie <brandon@cold.org>
2025-05-17 10:47:19 -0600

    .

--- lib/rivet/ecto/collection/model.ex
+++ lib/rivet/ecto/collection/model.ex
@@ -14,13 +14,17 @@ defmodule Rivet.Ecto.Collection.Model do
 
   @spec validate_unique_constraints(Ecto.Changeset.t(), [{term(), keyword()} | term()]) ::
           Ecto.Changeset.t()
-  def validate_unique_constraints(chgset, [{key, opts} | rest]),
-    do: unique_constraint(chgset, key, opts) |> validate_unique_constraints(rest)
+  def validate_unique_constraints(chgset, [{key, opts} | rest]) do
+    IO.inspect({key, opts, chgset}, label: "unique_constraint")
+    unique_constraint(chgset, key, opts)
+    |> IO.inspect(label: "AFTER/2")
+    |> validate_unique_constraints(rest)
+  end
 
   def validate_unique_constraints(chgset, [key | rest]) do
     IO.inspect({key, chgset}, label: "unique_constraint")
     unique_constraint(chgset, key)
-    |> IO.inspect(label: "AFTER")
+    |> IO.inspect(label: "AFTER/1")
     |> validate_unique_constraints(rest)
   end
 
